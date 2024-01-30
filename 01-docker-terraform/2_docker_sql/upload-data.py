52ca1743475061933f70db06de54fff29ecc343f
maiqkhan <33614138+maiqkhan@users.noreply.github.com>
2024-01-30 01:19:00 +0000

    reformat taxi dataframe to correct order

--- 01-docker-terraform/2_docker_sql/upload-data.py
+++ 01-docker-terraform/2_docker_sql/upload-data.py
@@ -22,7 +22,16 @@ def main(params):
     print(csv_name)
     df = pd.read_csv(csv_name, compression='gzip')
 
-
+    print(df.columns)
+    df = df.rename(columns={'lpep_pickup_datetime': 'tpep_pickup_datetime',
+                       'lpep_dropoff_datetime': 'tpep_dropoff_datetime'})
+    
+    df = df[['VendorID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime',
+       'passenger_count', 'trip_distance', 'RatecodeID', 'store_and_fwd_flag',  'PULocationID', 
+       'DOLocationID',  'payment_type',   'fare_amount', 'extra', 'mta_tax',
+       'tip_amount', 'tolls_amount', 'improvement_surcharge', 'total_amount', 'congestion_surcharge',  'ehail_fee', 
+        'trip_type' ]].copy()
+    print(df.columns)
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 

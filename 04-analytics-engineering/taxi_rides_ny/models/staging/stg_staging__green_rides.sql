3d6e158dfe17e85ad044803d8554e2d0f5ad0533
Mustafa Khan <mkhan2431@gmail.com>
2024-03-21 23:01:08 +0000

    create staging models

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_staging__green_rides.sql
@@ -0,0 +1,35 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'green_rides') }}
+
+),
+
+renamed as (
+
+    select
+        vendorid,
+        tpep_pickup_datetime,
+        tpep_dropoff_datetime,
+        passenger_count,
+        trip_distance,
+        ratecode_id,
+        store_and_fwd_flag,
+        pulocation_id,
+        dolocation_id,
+        payment_type,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        improvement_surcharge,
+        total_amount,
+        congestion_surcharge
+
+    from source
+
+)
+
+select * from renamed

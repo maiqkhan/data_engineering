e214767b9c9dfe167f55ced4ffb02bd446c1c2d3
Mustafa Khan <mkhan2431@gmail.com>
2024-03-22 03:10:18 +0000

    add dev limit to staging models

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_staging__green_rides.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_staging__green_rides.sql
@@ -1,37 +1,57 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with 
 
-source as (
+tripdata as (
 
-    select * from {{ source('staging', 'green_rides') }}
-
-),
-
-renamed as (
-
-    select
-        {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
-        vendorid,
-        tpep_pickup_datetime,
-        tpep_dropoff_datetime,
-        passenger_count,
-        trip_distance,
-        ratecode_id,
-        store_and_fwd_flag,
-        pulocation_id,
-        dolocation_id,
-        payment_type,
-        {{get_payment_type_description('payment_type')}} as payment_type_description,
-        fare_amount,
-        extra,
-        mta_tax,
-        tip_amount,
-        tolls_amount,
-        improvement_surcharge,
-        total_amount,
-        congestion_surcharge
-
-    from source
+    select *,
+        row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+     from {{ source('staging', 'green_rides') }}
+     where vendorid is not null
 
 )
 
-select * from renamed
+select
+    --identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
+    cast(vendorid as integer) as vendorid,
+    cast(ratecode_id as integer) as ratecode_id,
+    cast(pulocation_id as integer) as pulocation_id,
+    cast(dolocation_id as integer) as dolocation_id,
+    
+    -- timestamps
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    store_and_fwd_flag,
+    cast(passenger_count as integer) as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    cast(passenger_count as integer) as passenger_count,
+
+    
+    --payment_info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    coalesce(cast(payment_type as integer), 0) as payment_type
+    {{get_payment_type_description('payment_type')}} as payment_type_description
+
+from tripdata
+where rn = 1
+
+
+
+{% if var('is_test_run', default=true)%}
+
+    limit 100 
+
+{% endif %}
